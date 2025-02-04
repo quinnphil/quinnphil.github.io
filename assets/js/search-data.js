6dc146b93348127154927e10bb1f8e63794019c4
quinnphil <phil@philquinn.co.uk>
2025-02-04 11:56:09 +0000

    Deploying to gh-pages from @ quinnphil/quinnphil.github.io@a36dd21f751589074610db4e2480e48c7ad91d02 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -79,7 +79,18 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-a-post-with-image-galleries",
+            },{id: "post-powershell-bulk-rename-file-extension",
+      
+        title: "Powershell - Bulk rename file extension",
+      
+      description: "Powershell command for bulk changing file extensions.",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/powershell-bulk-rename/";
+        
+      },
+    },{id: "post-a-post-with-image-galleries",
       
         title: "a post with image galleries",
       
@@ -374,17 +385,6 @@ ninja.data = [{
       handler: () => {
         
           window.location.href = "/blog/2015/math/";
-        
-      },
-    },{id: "post-powershell-bulk-rename-file-extension",
-      
-        title: "Powershell - Bulk rename file extension",
-      
-      description: "Powershell command for bulk changing file extensions.",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2015/powershell-bulk-rename/";
         
       },
     },{id: "post-a-post-with-code",
